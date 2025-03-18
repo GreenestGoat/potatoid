f5ff76bc16fcf43a7888b812aa73eee6179fc84a
Fin :3 <118539051+GreenestGoat@users.noreply.github.com>
2025-03-18 20:25:52 +0000

    Update potatoid.js

--- potatoid.js
+++ potatoid.js
@@ -1,4 +1,13 @@
-export const potatoid = (len = 10, base = 36) =>
+export const potatoid = (len = 10, b = 36, c) =>
   crypto
     .getRandomValues(new Uint8Array(len))
-    .reduce((s, b) => s + (b % base).toString(base), "")
+    .reduce(
+      (s, x) =>
+        s +
+        (typeof b === "string"
+          ? b[x % b.length]
+          : c
+          ? c[x % c.length]
+          : (x % (b = Math.min(Math.max(b, 2), 36))).toString(b)),
+      ""
+    )
