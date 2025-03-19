d293bcd46aa764d4f2916161e41c6f0e0ee35269
Brick_Wall <118539051+GreenestGoat@users.noreply.github.com>
2025-03-19 10:12:34 +0000

    add esbuild

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,93 @@
-import typescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
+import typescript from "@rollup/plugin-typescript";
+import { minify } from "rollup-plugin-esbuild-minify";
+import babel from "@rollup/plugin-babel";
+import nodeResolve from "@rollup/plugin-node-resolve";
+import replace from "@rollup/plugin-replace";
+import versionInjector from "rollup-plugin-version-injector";
+import prettier from "rollup-plugin-prettier";
 
-export default {
-  input: 'src/potatoid.ts',
-  output: [
-    { file: 'dist/potato.js', format: 'es' },
-    { file: 'dist/potatoid.min.js', format: 'es', plugins: [terser()] }
+const babelConfig = {
+  babelHelpers: "bundled",
+  extensions: [".ts", ".js"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          esmodules: true,
+        },
+        modules: false,
+        bugfixes: true,
+        loose: true,
+      },
+    ],
   ],
-  plugins: [typescript()]
 };
+
+export default [
+  {
+    input: "src/potatoid.ts",
+    output: {
+      dir: "dist",
+      format: "es",
+      preserveModules: true,
+      entryFileNames: "[name].js",
+      sourcemap: true,
+    },
+    plugins: [
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
+      typescript(),
+      nodeResolve(),
+      babel(babelConfig),
+      versionInjector({
+        injectInComments: {
+          fileRegexp: /\.(js|ts)$/,
+          tag: "potatoid @{version}",
+        },
+      }),
+      prettier({
+        parser: "babel",
+        tabWidth: 2,
+        singleQuote: true,
+        bracketSpacing: true,
+      }),
+    ],
+    treeshake: {
+      moduleSideEffects: false,
+    },
+  },
+  {
+    input: "src/potatoid.ts",
+    output: {
+      file: "dist/potatoid.min.js",
+      format: "es",
+      sourcemap: true,
+    },
+    plugins: [
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
+      typescript(),
+      nodeResolve(),
+      babel(babelConfig),
+      versionInjector({
+        injectInComments: {
+          fileRegexp: /\.(js|ts)$/,
+          tag: "potatoid @{version}",
+        },
+      }),
+      minify({
+        minify: true,
+        legalComments: "none",
+        logLevel: "warning",
+      }),
+    ],
+    treeshake: {
+      moduleSideEffects: false,
+    },
+  },
+];
